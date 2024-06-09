bfcf7c9694eb9495bb53cb70a2ad82489580ba2e
Miguel <migueortiz0125@gmail.com>
2024-06-09 09:42:41 -0700

    completed about section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero';
 import AOS from "aos";
 import "aos/dist/aos.css";
+import About from './components/About/About';
 
 
 const App = () => {
@@ -35,6 +36,7 @@ const App = () => {
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
       <Hero theme={theme} />
+      <About />
     </div>
   )
 }
---
+++ src/components/About/About.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import car1 from "../../assets/car1.png"
+
+const About = () => {
+    return (
+        <div className="dark:bg-dark bg-slate-100 dark:text-white duration-300 sm:min-h-[600px] sm:grid sm:place-items-center">
+            <div className="container">
+                <div className="grid grid-cols-1 sm:grid-cols-2 place-items-center">
+                    <div data-aos="slide-right" data-aos-duration="1500">
+                        <img src={car1} alt="" className="sm:scale-105 sm:-translate-x-11 max-h-[300px] drop-shadow-[2px_10px_6px_rgba(0,0,0,0.50)]"/>
+                    </div>
+                    <div>
+                        <div className="space-y-5 sm:p-16 pb-6">
+                            <h1 className="text-3xl sm:text-4xl font-bold font-sans" data-aos="fade-up">
+                                About Us
+                            </h1>
+                            <p data-aos="fade-up" className="leading-8 tracking-wide">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, at debitis. Rem, aliquid debitis?
+                            </p>
+                            <p data-aos="fade-up">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, amet!
+                            </p>
+                            <button data-aos="fade-up" className="button-outline">
+                                Get Started
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About
