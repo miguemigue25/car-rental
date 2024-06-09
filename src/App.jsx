76d4cf26b6a6a1b5c13e2d474468684a7ee393f5
Miguel <migueortiz0125@gmail.com>
2024-06-08 22:58:16 -0700

    hero completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
+import Hero from './components/Hero/Hero';
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme")
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
+      <Hero theme={theme}/>
     </div>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import car from '../../assets/car.png'
+import yellow from '../../assets/banner-car.png'
+
+
+const Hero = ({theme}) => {
+  return (
+    <div className="dark:bg-black dark:text-white duration-300 relative -z-20">
+      <div className="container min-h-[620px] flex">
+        <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
+          <div className="order-1 sm:order-2">
+            <img src={theme === "dark" ? car : yellow} alt='' className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
+          </div>
+          <div className="order-2 sm:order-1 space-y-5 sm:pr-32">
+            <p className="text-primary text-2xl">Effortless</p>
+            <h1 className="text-5xl lg:text-7xl font-semibold font-serif">Car Detail</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius reiciendis similique eveniet illum libero voluptatem magnam, labore veniam illo optio aspernatur iure sed animi!</p>
+            <button className="btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
