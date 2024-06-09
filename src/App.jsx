192712ed76e8149837e606f90ee85ce4f60da2dd
Miguel <migueortiz0125@gmail.com>
2024-06-09 08:41:17 -0700

    added animation to hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme")
@@ -12,16 +15,26 @@ const App = () => {
     if (theme === "dark") {
       element.classList.add("dark");
       localStorage.setItem("theme", "dark");
-  } else {
+    } else {
       element.classList.remove("dark");
       localStorage.setItem("theme", "light");
     }
-  }, [theme])
+  }, [theme]);
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
     <div>
       <Navbar theme={theme} setTheme={setTheme} />
-      <Hero theme={theme}/>
+      <Hero theme={theme} />
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,21 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import car from '../../assets/car.png'
 import yellow from '../../assets/banner-car.png'
+import AOS from 'aos'
 
 
-const Hero = ({theme}) => {
+const Hero = ({ theme }) => {
+  useEffect(() => {
+    AOS.refresh();
+  });
+
   return (
     <div className="dark:bg-black dark:text-white duration-300 relative -z-20">
       <div className="container min-h-[620px] flex">
         <div className="grid place-items-center grid-cols-1 sm:grid-cols-2">
-          <div className="order-1 sm:order-2">
-            <img src={theme === "dark" ? car : yellow} alt='' className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
+          <div className="order-1 sm:order-2" data-aos="zoom-in" data-aos-duration="1500" data-aos-once="false">
+            <img src={theme === "dark" ? car : yellow} alt=''
+              className="relative -z-10 max-h-[600px] sm:scale-125 drop-shadow-[2px_20px_6px_rgba(0,0,0,0.50)]" />
           </div>
           <div className="order-2 sm:order-1 space-y-5 sm:pr-32">
-            <p className="text-primary text-2xl">Effortless</p>
-            <h1 className="text-5xl lg:text-7xl font-semibold font-serif">Car Detail</h1>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius reiciendis similique eveniet illum libero voluptatem magnam, labore veniam illo optio aspernatur iure sed animi!</p>
-            <button className="btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80">
+            <p className="text-primary text-2xl" data-aos="fade-up">
+              Effortless
+            </p>
+            <h1 className="text-5xl lg:text-7xl font-semibold font-serif" data-aos="fade-up" data-aos-delay="600">
+              Car Detail
+            </h1>
+            <p data-aos="fade-up" data-aos-delay="1000">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius reiciendis similique eveniet illum libero voluptatem magnam, labore veniam illo optio aspernatur iure sed animi!
+            </p>
+            <button data-aos="fade-up" data-aos-delay="1500" onClick={() => {
+              AOS.refreshHard();
+            }}
+              className="btn bg-primary text-black px-6 py-2 rounded-md hover:bg-primary/80"
+            >
               Get Started
             </button>
           </div>
